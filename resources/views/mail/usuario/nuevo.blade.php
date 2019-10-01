a3e7ca2d9ec5da7cb007a58afb659f17606c7e46
Leandro Moreno <123857@gmail.com>
2019-10-01 11:26:32 -0500

    Se crea el correo de usuario nuevo

--- resources/views/mail/usuario/nuevo.blade.php
+++ resources/views/mail/usuario/nuevo.blade.php
@@ -1,12 +1,25 @@
 @component('mail::message')
-# Introduction
+*Cordial saludo,*
 
-The body of your message.
+_Gracias por asistir a los Seminarios Virtuales de Excelencia de la Facultad de Ingeniería de la Universidad de los Andes._
 
-@component('mail::button', ['url' => ''])
-Button Text
+_Si no tiene cuenta para descargar su certificado debe restablecer la contraseña_
+<?php
+$url=url('/login');
+?>
+@component('mail::button', ['url' => $url])
+Restablezca la contraseña AQUÍ
+@endcomponent
+<?php
+$url=url('/login');
+?>
+_Si tiene cuenta,_
+@component('mail::button', ['url' => $url])
+Descargue su certificado AQUÍ
 @endcomponent
 
-Thanks,<br>
-{{ config('app.name') }}
+_Atentamente,_<br>
+_Facultad de Ingeniería_
+_Universidad de Los Andes_
+_Bogotá D.C., Colombia_
 @endcomponent
